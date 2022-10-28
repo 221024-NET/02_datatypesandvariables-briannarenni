656d3575de8ae4fc921bce8a2f27a40c20e38985
Brianna Renni <briannaxrene@icloud.com>
2022-10-27 23:02:26 -0700

    Add variables

--- DataTypeAndVariables/Program.cs
+++ DataTypeAndVariables/Program.cs
@@ -8,18 +8,18 @@ namespace DataTypeAndVariables
       {
           Console.WriteLine("Hello World!");
 
-          byte myByte;
-          sbyte mySbyte;
-          int myInt;
-          uint myUint;
-          short myShort;
-          ushort myUShort;
-          float myFloat;
-          double myDouble;
-          char myCharacter;
-          bool myBool;
-          string myText;
-          string numText;
+          byte myByte = 235;
+          sbyte mySbyte = -113;
+          int myInt = 3450;
+          uint myUint = 3803320;
+          short myShort = -3892;
+          ushort myUShort = 5;
+          float myFloat = 5.4f;
+          double myDouble = 96.45;
+          char myCharacter = 'a';
+          bool myBool = true;
+          string myText = "Hello World";
+          string numText = Convert.ToString(25);
       }
 
       public static int Text2Num(string numText)
