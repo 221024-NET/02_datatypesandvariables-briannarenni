55a329aedf2c77690629e82acd9bca17f610ec2f
Brianna Renni <briannaxrene@icloud.com>
2022-10-27 23:32:31 -0700

    Passed tests

--- DataTypeAndVariables/Program.cs
+++ DataTypeAndVariables/Program.cs
@@ -2,28 +2,63 @@ using System;
 
 namespace DataTypeAndVariables
 {
-    public class Program
-    {
-      public static void Main(string[] args)
-      {
-          Console.WriteLine("Hello World!");
-
+    public class Program {
+      public static void Main(string[] args) {
           byte myByte = 235;
-          sbyte mySbyte = -113;
-          int myInt = 3450;
-          uint myUint = 3803320;
-          short myShort = -3892;
-          ushort myUShort = 5;
-          float myFloat = 5.4f;
-          double myDouble = 96.45;
-          char myCharacter = 'a';
-          bool myBool = true;
-          string myText = "Hello World";
-          string numText = Convert.ToString(25);
-      }
+          Console.WriteLine(myByte);
+          Console.WriteLine("\n");
 
-      public static int Text2Num(string numText)
-      {
+          sbyte mySbyte = -113;
+          Console.WriteLine(mySbyte);
+          Console.WriteLine("\n");
+
+          int myInt = 3450;
+          Console.WriteLine(myInt);
+          Console.WriteLine("\n");
+
+          uint myUint = 3803320;
+          Console.WriteLine(myUint);
+          Console.WriteLine("\n");
+
+          short myShort = -3892;
+          Console.WriteLine(myShort);
+          Console.WriteLine("\n");
+
+          ushort myUShort = 5;
+          Console.WriteLine(myUShort);
+          Console.WriteLine("\n");
+
+          float myFloat = 5.4f;
+          Console.WriteLine(myFloat);
+          Console.WriteLine("\n");
+
+          double myDouble = 96.45;
+          Console.WriteLine(myDouble);
+          Console.WriteLine("\n");
+
+          char myCharacter = 'a';
+          Console.WriteLine(myCharacter);
+          Console.WriteLine("\n");
+
+          bool myBool = true;
+          Console.WriteLine(myBool);
+          Console.WriteLine("\n");
+
+          string myText = "Hello World";
+          Console.WriteLine(myText);
+          Console.WriteLine("\n");
+
+          string text = "I control text.";
+          string numText = "25";
+          Console.WriteLine(text);
+          Console.WriteLine("\n");
+          Console.WriteLine(Text2Num(numText));
+
+      }
+       public static int Text2Num(string numText) {
+        int result;
+        bool success = int.TryParse(numText, out result);
+        return result;
         throw new NotImplementedException();
       }
     }
